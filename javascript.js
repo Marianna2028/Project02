87fe00fc69234a83d9b757b20167e4b612193355
Marianna2028 <marianna.regina24@my.stjohns.edu>
2025-10-22 12:33:04 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector("#new-task").onsubmit = function (event) {
       
-      event.preventDefault(); // stops the page from refreshing
+        event.preventDefault(); // stops the page from refreshing
 
-      const li = document.createElement('li'); 
-      li.innerHTML = document.querySelector('#task').value;
-      document.querySelector("#tasks_list").appendChild(li);
-      document.querySelector("#task").value = ''; // clear input
+        const li = document.createElement('li'); 
+        let task_text = document.querySelector('#task').value.trim();
+        if (task_text === "") return; // don’t add empty tasks
+
+        let new_task_html = `
+            <span>${task_text}</span>
+            <button class="remove">Remove Task</button>
+        `;
+        li.innerHTML = new_task_html;
+
+        // Remove task functionality
+        li.querySelector('.remove').addEventListener('click', function() {
+            li.remove();
+        });
+
+        document.querySelector("#tasks_list").appendChild(li);
+        document.querySelector("#task").value = ''; // clear input
     }
 });
 
