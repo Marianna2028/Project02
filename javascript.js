457433b6506e0cc931422bc3eb1181441d4bc3d9
Marianna2028 <marianna.regina24@my.stjohns.edu>
2025-10-22 12:21:55 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector("#new-task").onsubmit = function () {
       
-      cost li = document.createElement('li');
-      li.innerHTML = document.querySelector('#task').value 
-      document.querySelector("#tasks_list").appendli;
-      document.querySelector("#task").value = '';
+      const li = document.createElement('li');
+      li.innerHTML = document.querySelector('#task').value; 
+      document.querySelector("#tasks_list").appendChild(li); 
+      document.querySelector("#task").value = ''; 
 
-        return false;
+      return false; 
     }
-  
 });
-
 
